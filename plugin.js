0dd3d65dece0165095cf81cc744c62b94d7024ca
Phoebe Li <phoebeli@Phoebes-MacBook-Pro.local>
2015-05-24 19:22:21 -0400

    update roadmap in plugin.js to compare each css individually

--- plugin.js
+++ plugin.js
@@ -16,13 +16,11 @@ DemoPlugin.prototype.apply = function(compiler) {
         //check if filepath ends in css
           //split at ! and fs
           //concat file path to css array
-          //check if module is first css
-            //if not, splice out module
         //check if filepath does not contain node_modules
           //concat path to content array
       //pass css and content array into kenny's function
-      //store result in uncss string
-      //go back to first css module and splice in our code
+      //store results in uncss array
+      //update css modules accordingly
 
   }); //end compiler.plugin
 
