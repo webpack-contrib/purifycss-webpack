adbe3e5b40090fe2d4f02a8865a8e479260ff62b
Phoebe Li <phoebeli@Phoebes-MacBook-Pro.local>
2015-05-26 16:29:58 -0400

    integrated plugin w/ purify css module

--- content.js
+++ content.js
@@ -1,3 +1,3 @@
 module.exports = "It works from content.js.<html></html>";
 
-console.log('some stuff');
+console.log('body');
--- plugin.js
+++ plugin.js
@@ -1,4 +1,6 @@
 var fs = require('fs');
+var purify = require('./node_modules/purify-css/purifycss.js');
+var S = require('string');
 
 var DemoPlugin = function(output, options) {
   this.output = output;
@@ -12,16 +14,47 @@ DemoPlugin.prototype.apply = function(compiler) {
   compiler.plugin('compilation', function(compilation) {
 
     compilation.plugin("optimize-modules", function(modules) {
-      //iterate over modules
-        //check if filepath ends in css
-          //split at ! and fs
-          //concat file path to css array
+      var content = [];
+      var css = [];
+
+      //get all html/js contents
+      modules.forEach(function(module) {
         //check if filepath does not contain node_modules
+        var filepath = module._source._name;
+        var sourcecode = module._source._value;
+
+        if (!S(filepath).contains('node_modules')) {
           //concat path to content array
-      //pass css and content array into kenny's function
-      //store results in uncss array
-      //update css modules accordingly
+          content.push(sourcecode);
+        }
+      })
 
-  }); //end compiler.plugin
+      content = content.join("");
 
-};
+      modules.forEach(function(module) {
+        //iterate over modules
+        var filepath = module._source._name;
+        var sourcecode = module._source._value;
+
+        //skip over files loaded by style-loader
+        if (S(filepath).contains('!') && !S(filepath).contains('style-loader')) {
+          //parse sourcecode for only css
+          var css = S(sourcecode).between('module.id, ',', ""]);').s;
+
+          //pass css and content array into kenny's function
+          var uncss = purify(content, eval(css), {write: false, minify: false});
+
+          //stringify purified css
+          uncss = JSON.stringify(uncss);
+
+          //insert back into module
+          module._source._value = sourcecode.replace(css, uncss)
+          console.log('-----MODULE------',module._source._value);
+        }
+        
+      }) //end modules.forEach
+
+    }); //end compiler.plugin
+
+  });
+}
