f666f1920599022caad754dbdc617012c4ab0907
Kenny Tran <kenny8tran@gmail.com>
2015-06-06 16:47:44 -0700

    Add preliminary functionality for plugin

--- index.js
+++ index.js
@@ -4,30 +4,72 @@ var DemoPlugin = function(rootHtml) {
 
 module.exports = DemoPlugin;
 
-DemoPlugin.prototype.apply = function(compiler) {
-  compiler.plugin('this-compilation', function(compilation) {
+var isCss = function(module){
+  console.log(module._source._name);
+  return module._source && module._source._value && 
+         module._source._name.indexOf('.css') > -1 &&
+         module._source._name.indexOf('node_modules') === -1;
+};
 
-    var content = "";
+var isContent = function(module){
+  return module._source && module._source._value && 
+         module._source._name.indexOf('.css') === -1 &&
+         module._source._name.indexOf('node_modules') === -1;
+};
+
+var getContentHash = function(compilation){
+  return compilation.chunks.reduce(function(contentHash, chunk){
+    var modules = chunk.modules;
+    var content = modules.reduce(function(total, m){
+      if(isContent(m)){
+        total += m._source._value;
+      }
+
+      return total;
+    }, "");
+
+    contentHash[chunk.name] = content;
+    return contentHash;
+  }, {});
+};
+
+var getCssModules = function(modules){
+  return modules.filter(function(module){
+    return isCss(module);
+  });
+};
+
+DemoPlugin.prototype.apply = function(compiler) {
+  var chunkContent;
+  var cssModules;
+
+  compiler.plugin('this-compilation', function(compilation) {
+    compilation._purifycss_callback = function(){
+      if(cssModules.length === 0){
+        return;
+      }
+
+      var nextModule = cssModules.pop();
+      compilation._purifycss_content = '';
+
+      nextModule.chunks.forEach(function(chunk){
+        compilation._purifycss_content += chunkContent[chunk.name] + ' ';
+      });
+
+      compilation.rebuildModule(nextModule, function(err){
+        if(err){
+          console.log(err);
+        }
+
+        compilation._purifycss_callback();
+      });
+    };
 
     compilation.plugin("optimize-modules", function(modules) {
-
-      modules.forEach(function(m){
-        if(m._source && m._source._value && 
-           m._source._name.indexOf('.css') === -1 &&
-           m._source._name.indexOf('node_modules') === -1){
-          content += m._source._value;
-        }
-      });
-
-      compilation.__content = content;
-
-      modules.forEach(function(m){
-        if(m._source && m._source._name.indexOf('.css') > -1){
-          compilation.rebuildModule(m, function(err){
-            if(err) console.log(err);
-          });
-        }
-      });
+      chunkContent = getContentHash(compilation);
+      cssModules = getCssModules(modules);
+      console.log(cssModules);
+      compilation._purifycss_callback();
     });
   });
 };
