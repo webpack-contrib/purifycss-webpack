a992f9972c8461c16a77766ecaddea5d550e4a40
Kenny Tran <kenny8tran@gmail.com>
2015-06-16 07:02:09 -0700

    Change from plugin to loader

--- loader.js
+++ loader.js
@@ -2,7 +2,6 @@ var purify = require('purify-css');
 
 module.exports = function(input){
   if(typeof this._compilation._purifycss_content === 'undefined'){
-    console.log('SEE ME PLEASE');
     return input;
   }
   
