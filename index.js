a69ecdd0cb9b64f3b7c46a48fd9d09254d18047e
Nenad Panic <nenad.panic@clariness.com>
2016-04-17 19:06:12 +0200

    support purify css by chunks

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ module.exports.prototype.apply = function(compiler) {
         self.purifyOptions.output = false;
         // Path/files to check. If none supplied, an empty array will do.
         self.paths = self.userOptions.paths || [];
+        self.entryPaths = self.userOptions.entryPaths || [];
         // Additional extensions to scan for. This is kept minimal, for obvious reasons.
         // We are not opinionated...
         self.resolveExtensions = self.userOptions.resolveExtensions || compiler.options.resolve.extensions;
@@ -35,24 +36,80 @@ module.exports.prototype.apply = function(compiler) {
         }, []);
 
         compilation.plugin("additional-assets", function(cb){
-            // Look for additional JS/HTML stuff.
-            for(var key in compilation.fileDependencies) {
-                var file = compilation.fileDependencies[key];
-                var ext = path.extname(file);
-                if (self.resolveExtensions.indexOf(ext) > -1) files.push(file);
+            if (compilation.chunks.length === 1 && compilation.chunks[0].name === 'main') {
+                // main chunk
+                // Look for additional JS/HTML stuff.
+                for(var key in compilation.fileDependencies) {
+                    var file = compilation.fileDependencies[key];
+                    var ext = path.extname(file);
+                    if (self.resolveExtensions.indexOf(ext) > -1) files.push(file);
+                }
+
+                // Look for purifyable CSs...
+                for(var key in compilation.assets) {
+                    if(/\.css$/i.test(key)) {
+                        // We found a CSS. So purify it.
+                        var asset = compilation.assets[key];
+                        executePurification(files, asset, key);
+                    }
+                }
+            } else {
+                // multiple entry chunks
+                var cssAsset;
+                var assets = Object.keys(compilation.assets);
+
+                compilation.chunks.forEach(function (chunk, i) {
+                    // extract html chunk files
+                    var key;
+                    var error = false;
+
+                    if (self.entryPaths[chunk.name]) {
+                        var chunkFiles = self.entryPaths[chunk.name].reduce(function (results, p) {
+                            return results.concat(glob(path.join(self.basePath, p)));
+                        }, []);   
+                    } else {
+                        console.log('Wrong entryPath for ' + chunk.name);
+                        error = true;
+                    }
+
+                    // filter chunk modules for additional files to include ex. .js, .es6,...
+                    var chunkModules = chunk.modules.filter(function (module) {
+                        var ext = path.extname(module.resource);
+                        return self.resolveExtensions.indexOf(ext) > -1;
+                    });
+
+                    // include additional modules
+                    chunkFiles.concat(chunkModules);
+
+                    // find css asset
+                    for (var k = 0; k < assets.length; k++){
+                        if (assets[k].indexOf(chunk.name) > -1 && path.extname(assets[k]) === '.css') {
+                            cssAsset = compilation.assets[assets[k]];
+                            key = assets[k];
+                            break;
+                        }
+                    }
+
+                    if (cssAsset && !error) {
+                        if (self.purifyOptions.info) {
+                            console.log(chunk.name);
+                        }
+
+                        executePurification(chunkFiles, cssAsset, key);
+                    } else {
+                        console.log('No CSS for chunk: ' + chunk.name);
+                    }
+                });
             }
 
-            // Look for purifyable CSs...
-            for(var key in compilation.assets) {
-                if(/\.css/i.test(key)) {
-                    // We found a CSS. So purify it.
-                    var asset = compilation.assets[key];
-                    var css = asset.source();
-                    var newCss = new ConcatSource();
-                    newCss.add(purify(files, css, self.purifyOptions));
-                    compilation.assets[key] = newCss;
-                }
+            function executePurification(files, cssAsset, key) {
+                var css = cssAsset.source();
+                var newCss = new ConcatSource();
+
+                newCss.add(purify(files, css, self.purifyOptions));
+                compilation.assets[key] = newCss;
             }
+
             cb();
         });
     });
