ed0f6063a57fad839b66f42a0452a34e5c99d812
Juho Vepsalainen <bebraw@gmail.com>
2017-01-10 10:31:11 +0200

    chore - Drop prototype bit and this to simplify further

--- src/index.js
+++ src/index.js
@@ -3,35 +3,34 @@ const parse = require('./parse');
 const purifyCSS = require('./purify-css');
 
 module.exports = function PurifyPlugin(options) {
-  // Store the user's options
-  this.userOptions = options;
-};
+  return {
+    apply(compiler) {
+      compiler.plugin('this-compilation', (compilation) => {
+        const paths = parse.paths(options.paths);
 
-module.exports.prototype.apply = function apply(compiler) {
-  compiler.plugin('this-compilation', (compilation) => {
-    const paths = parse.paths(this.userOptions.paths);
+        // Additional extensions to scan for. This is kept minimal.
+        const resolveExtensions = (
+          options.resolveExtensions || compiler.options.resolve.extensions
+        );
 
-    // Additional extensions to scan for. This is kept minimal.
-    const resolveExtensions = (
-      this.userOptions.resolveExtensions || compiler.options.resolve.extensions
-    );
+        compilation.plugin('additional-assets', (cb) => {
+          // Check only if there is one chunk and if paths are an array
+          if (compilation.chunks.length === 1 || Array.isArray(paths)) {
+            purifyCSS(
+              // Look for additional files
+              paths.concat(
+                searchAdditionalFiles(compilation.fileDependencies, resolveExtensions)
+              ),
+              compilation.assets,
+              parse.options(options.purifyOptions)
+            ).forEach((o) => {
+              compilation.assets[o.key] = o.value;
+            });
+          }
 
-    compilation.plugin('additional-assets', (cb) => {
-      // Check only if there is one chunk and if paths are an array
-      if (compilation.chunks.length === 1 || Array.isArray(paths)) {
-        purifyCSS(
-          // Look for additional files
-          paths.concat(
-            searchAdditionalFiles(compilation.fileDependencies, resolveExtensions)
-          ),
-          compilation.assets,
-          parse.options(this.userOptions.purifyOptions)
-        ).forEach((o) => {
-          compilation.assets[o.key] = o.value;
+          cb();
         });
-      }
-
-      cb();
-    });
-  });
+      });
+    }
+  };
 };
