e1417e9e15e2748220184897210692fb71d206d2
Juho Vepsalainen <bebraw@gmail.com>
2017-01-10 11:06:52 +0200

    chore - Tidy up

--- src/purify-css.js
+++ src/purify-css.js
@@ -1,15 +1,11 @@
 const purify = require('purify-css');
 const ConcatSource = require('webpack-sources').ConcatSource;
 
-function purifyCSS(files, assets, purifyOptions) {
-  return assets.map(({ key, asset }) => {
-    const css = asset.source();
-    const source = new ConcatSource();
-
-    source.add(purify(files, css, purifyOptions));
-
-    return { key, source };
-  })
-}
-
-module.exports = purifyCSS;
+module.exports = function purifyCSS(files, assets, purifyOptions) {
+  return assets.map(({ key, asset }) => (
+    {
+      key,
+      source: new ConcatSource(purify(files, asset.source(), purifyOptions))
+    }
+  ));
+};
